6c703f599ee1e665a130d4aca4e795439156e867
Daniel Doherty <d.doherty@kainos.com>
2020-12-04 12:17:19 +0000

    HTML and Nunjucks added for adding a course

--- public/src/controllers/course.controller.js
+++ public/src/controllers/course.controller.js
@@ -4,6 +4,11 @@ exports.index = async (_req, res) => {
   res.render('index.njk', { courses: await Course.getAll() })
 };
 
+// exports.show = (_req, res) => {
+//     res.render('index.njk');
+// };
+
+
 exports.show = (_req, res) => {
-    res.render('index.njk');
+    res.render('addcourse.njk');
 };
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,7 @@ const courseController = require("../public/src/controllers/course.controller.js
 /* GET home page. */
 router.get('/', courseController.index);
 
+/* GET home page. */
+router.get('/addcourse', courseController.show);
+
 module.exports = router;
