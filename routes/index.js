28b9b03893c9efbaa765a67e9efb33e6087db3d8
Soorya <soorya11@hotmail.co.uk>
2020-12-04 10:30:02 +0000

    remove gitignore files

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-const courseController = require("../public/src/controllers/course.controller.js");
-
-/* GET home page. */
-router.get('/', courseController.index);
-
-module.exports = router;
